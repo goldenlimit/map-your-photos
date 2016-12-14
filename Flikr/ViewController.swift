d675f54104a5a66243b6b781076b3cc555cad406
Garima Dhakal <garimadhakal@CodeVlogs.local>
2016-12-13 23:15:45 -0800

    Initial Commit

---
+++ Flikr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flikr
+//
+//  Created by Garima Dhakal on 12/13/16.
+//  Copyright © 2016 Garima Dhakal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
